59d05b53d063f3b6fedb1483778f9c279ddfd5fd
kendofriendo <goadke@g.cofc.edu>
2021-04-12 17:43:54 -0400

    del solv add consts

--- js/challenge.js
+++ js/challenge.js
@@ -1,10 +1,13 @@
+
+Document.addEventListener("DOMContentLoaded", function() {
+	const minus = document.getElementById("minus")
+	const plus = document.getElementById("plus")
+	const heart = document.getElementById("heart")
+	const pause = document.getElementById("pause")
+	const ul = document.getElementById("ul")
+	const list = document.getElementById("#list")
+})
 // As a user, I should see the timer increment every second once the page has loaded.
-
-document.addEventListener("DOMContentLoaded", () => {
-	parseInt(document.getElementById("counter").innerText);
-
-});
-
 // As a user, I can manually increment and decrement the counter using the plus and minus buttons.
 // As a user, I can 'like' an individual number of the counter. I should see count of the number of 'likes' associated with that number.
 // As a user, I can pause the counter, which should
